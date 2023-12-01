4d2c96495a14f2377ea92f78e88e4a30aa6a2277
Denisolo <deniisolo@outlook.es>
2023-12-01 17:12:58 -0500

    estructura css

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 function Portafolio(){
-    document.getElementById("portafolio-main").style.display = "grid";
+    document.getElementById("portafolio-main").style.display = "grid"
     document.getElementById("container-main").style.display = "none"
+    document.getElementById("contacto-main").style.display = "none"
+    document.getElementById("aboutme-main").style.display = "none"
 }
 function Sobremi(){
     document.getElementById("aboutme-main").style.display = "grid"
     document.getElementById("portafolio-main").style.display = "none"
     document.getElementById("container-main").style.display = "none"
+    document.getElementById("contacto-main").style.display = "none"
 }
 
 function Contacto() {
 document.getElementById("contacto-main").style.display = "grid"
 document.getElementById("aboutme-main").style.display = "none"
 document.getElementById("container-main").style.display = "none"
+}
+function deniisolo(){
+    location.reload();
 }
